d058ef79ef789379f5ef3add00d96280629842fb
bailitusu <231820801@qq.com>
2018-05-24 16:58:54 +0800

    delete migration

--- src/main/resources/db.migration/V2018_05_23_creat_dbgreet_data_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE demo_data (
-  id          SERIAL  NOT NULL,
-  title       VARCHAR(200)           NOT NULL,
-  description VARCHAR(500)           NOT NULL,
-  CONSTRAINT pk_demo_data_id PRIMARY KEY (id),
-  CONSTRAINT uk_demo_data_title UNIQUE (title)
-);
